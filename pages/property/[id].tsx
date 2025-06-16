6fe2f4c9df205eb21a6ee8d11753b48f01254ecb
Joshua Emorwodia <joshuaemorwodia@gmail.com>
2025-06-16 12:52:52 +0100

    Fix: Add [id].tsx and remove duplicate folders

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,27 +1,9 @@
-// pages/property/[id].tsx
-import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
-import { useRouter } from "next/router";
-import PropertyDetail from "@/components/property/PropertyDetail";
-import BookingSection from "@/components/property/BookingSection";
-import ReviewSection from "@/components/property/ReviewSection";
+import PropertyDetail from '@/components/property/PropertyDetail';
 
 export default function PropertyPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
-
-  if (!property) return <p>Property not found</p>;
-
   return (
-    <div className="container mx-auto px-4 py-6 grid grid-cols-1 md:grid-cols-3 gap-6">
-      <div className="md:col-span-2">
-        <PropertyDetail property={property} />
-        <ReviewSection reviews={property.reviews} />
-      </div>
-      <div className="md:col-span-1">
-        <BookingSection price={property.price} />
-      </div>
-    </div>
+    <main>
+      <PropertyDetail />
+    </main>
   );
 }
