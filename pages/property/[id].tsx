fdad0640f3708b2abb14734a88389814cad7ae2f
Joshua Emorwodia <joshuaemorwodia@gmail.com>
2025-06-16 12:59:40 +0100

    Add [id].tsx to complete property detail page

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,9 +1,16 @@
+// pages/property/[id].tsx
+import React from 'react';
 import PropertyDetail from '@/components/property/PropertyDetail';
+import BookingSection from '@/components/property/BookingSection';
+import ReviewSection from '@/components/property/ReviewSection';
 
 export default function PropertyPage() {
   return (
-    <main>
+    <div>
       <PropertyDetail />
-    </main>
+      <BookingSection />
+      <ReviewSection />
+    </div>
   );
 }
+
